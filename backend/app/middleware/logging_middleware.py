fe5db952241640faca2388a51aef22e6fddd6cf6
viyash-24 <viyashviyashan2417@gmail.com>
2026-06-17 21:26:53 +0530

    logging class write

--- backend/app/middleware/logging_middleware.py
+++ backend/app/middleware/logging_middleware.py
@@ -2,3 +2,13 @@ import logging
 from fastapi import Request
 from starlette.middleware.base import BaseHTTPMiddleware
 import time
+
+logger = logging.getLogger("finova")
+
+class LoggingMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        start_time = time.time()
+        response = await call_next(request)
+        process_time = time.time() - start_time
+        logger.info(f"{request.method} {request.url.path} - {response.status_code} - {process_time:.4f}s")
+        return response
